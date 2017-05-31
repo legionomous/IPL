cb1bf57ad71941b0558e6e80f76184f1882d55b8
legionomous <legionomous@users.noreply.github.com>
2017-05-30 18:35:03 -0700

    Update No of matches.py

--- No of matches.py
+++ No of matches.py
@@ -7,9 +7,8 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as mlt
 import seaborn as sns
-matches=pd.read_csv('C:/Users/vrajm/Desktop/IPL/matches.csv')   
-delivery=pd.read_csv('C:/Users/vrajm/Desktop/IPL/deliveries.csv')
+matches=pd.read_csv('My Location/.../matches.csv')   
+delivery=pd.read_csv('My Location/.../deliveries.csv')
 print ("Total number of matches played:", len(matches))
-matches.drop(['umpire3'],axis=1,inplace=True)  #since all the values are NaN
-delivery.fillna(0,inplace=True)     #filling all the NaN values with 0
+
 
