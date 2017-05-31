545e748d06e1557e7086787748416a5f09072e2a
legionomous <legionomous@users.noreply.github.com>
2017-05-30 18:12:49 -0700

    Total Number of Matches played

---
+++ Number of Matches Played.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+matches=pd.read_csv('C:/Users/vrajm/Desktop/IPL/matches.csv')   
+delivery=pd.read_csv('C:/Users/vrajm/Desktop/IPL/deliveries.csv')
+print ("Total number of matches played:", len(matches))
+
