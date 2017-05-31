d93594141e837b495c926e3275f8496887b4da34
legionomous <legionomous@users.noreply.github.com>
2017-05-30 18:14:27 -0700

    Delete Number of Matches Played.py

--- Number of Matches Played.py
+++
@@ -1,9 +0,0 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-matches=pd.read_csv('C:/Users/vrajm/Desktop/IPL/matches.csv')   
-delivery=pd.read_csv('C:/Users/vrajm/Desktop/IPL/deliveries.csv')
-print ("Total number of matches played:", len(matches))
-
